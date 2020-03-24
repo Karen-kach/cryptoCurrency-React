80df19b64e3da3f6742e6aa6b2e675e383a8a271
Karen-kach <59180761+Karen-kach@users.noreply.github.com>
2020-03-24 23:25:36 +0400

    new list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch } from 'react-router-dom0';
+import Header from './components/common/Header';
+import List from './components/list/List';
 import './index.css';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+
+  )
+}
+
+ReactDOM.render( <App />,document.getElementById('root'));
